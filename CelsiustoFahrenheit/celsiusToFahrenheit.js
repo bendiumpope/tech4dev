3e3f9d06af876ebda12fee0c49ab6dcf0d1fbcfb
petrepan <peterawotola@gmail.com>
2021-05-26 16:29:53 +0100

    celseius to fahreinheit

---
+++ CelsiustoFahrenheit/celsiusToFahrenheit.js
@@ -0,0 +1,9 @@
+//This function takes in one number type as its parameter
+//The function converts convert from Celsius to Fahrenheit.
+//The formular to convert to fahrenheit is "temperature in Celsius times 9 / 5, plus 32".
+
+function convertToF(celsius) {
+  
+}
+
+module.exports = convertToF
